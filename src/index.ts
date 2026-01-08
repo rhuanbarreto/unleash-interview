152b0870ded0b69ed5f1f4c77764d8b7ce20125d
Rhuan Barreto <rhuan@barreto.work>
2026-01-08 19:43:53 -0300

    feat: adjust backend route

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,12 @@
 import { serve } from "bun";
-import index from "./index.html";
+import index from "./frontend/index.html";
+import { search } from "./api/searchService";
 
 const server = serve({
   routes: {
     // Serve index.html for all unmatched routes.
     "/*": index,
-
-    "/api/hello": {
-      async GET(req) {
-        return Response.json({
-          message: "Hello, world!",
-          method: "GET",
-        });
-      },
-      async PUT(req) {
-        return Response.json({
-          message: "Hello, world!",
-          method: "PUT",
-        });
-      },
-    },
-
-    "/api/hello/:name": async req => {
-      const name = req.params.name;
-      return Response.json({
-        message: `Hello, ${name}!`,
-      });
-    },
+    "/search/:term": (req) => Response.json(search(req.params.term)),
   },
 
   development: process.env.NODE_ENV !== "production" && {
