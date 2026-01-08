653dae2d32f705f2dd65330b3fec09b3619bc9f7
Rhuan Barreto <rhuan@barreto.work>
2026-01-08 20:15:49 -0300

    fix: adjust return objects to comply with requested return type

--- src/api/searchService.ts
+++ src/api/searchService.ts
@@ -3,12 +3,26 @@ import addresses from "src/api/data/adresses.json";
 
 const trie = new TrieSearch<(typeof addresses)[number]>(
   ["street", "postNumber", "city"],
-  { min: 3, idFieldOrFunction: (i) => i.postNumber + i.city + i.typeCode }
+  {
+    min: 3,
+    idFieldOrFunction: (i) => i.postNumber + i.city + i.typeCode,
+    ignoreCase: true,
+  }
 );
 
 trie.addAll(addresses);
 
 export function search(searchString: string): (typeof addresses)[number][] {
   if (searchString.length < 3) return [];
-  return trie.search(searchString);
+  return (
+    //FIXME: This type assertion is waiting for https://github.com/joshjung/trie-search/pull/57 to be removed.
+    (
+      trie.get(searchString, undefined, 20) as ((typeof addresses)[number] & {
+        $tsid: string;
+      })[]
+    ).map((a) => {
+      const { $tsid, ...rest } = a;
+      return rest;
+    })
+  );
 }
