d3a5f0f7f41d85ca47cbab49e9414bfc494fd7f3
Rhuan Barreto <rhuan@barreto.work>
2026-01-08 19:48:35 -0300

    feat: add search query

---
+++ src/frontend/queries/search.ts
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import type adresses from "src/api/data/adresses.json";
+import { useDebounce } from "../helpers/useDebounce";
+
+function fetchSearch(searchTerm: string) {
+  return fetch(`/search/${searchTerm}`).then(async (r) => {
+    if (!r.ok) {
+      const content = await r.text();
+      throw new Error(content);
+    }
+    return r.json() as Promise<typeof adresses>;
+  });
+}
+
+export function useSearch() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const debouncedSearchTerm = useDebounce(searchTerm);
+  const [results, setResults] = useState<
+    Awaited<ReturnType<typeof fetchSearch>>
+  >([]);
+
+  useEffect(() => {
+    if (debouncedSearchTerm.length < 3) return;
+    fetchSearch(debouncedSearchTerm).then((r) => setResults(() => r));
+  }, [debouncedSearchTerm]);
+
+  return { searchTerm, setSearchTerm, results } as const;
+}
