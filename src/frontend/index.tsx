034948773c63f3db66abe28388f7a57ed2b7a18f
Rhuan Barreto <rhuan@barreto.work>
2026-01-09 16:13:17 -0300

    feat: implement error boundary

--- src/frontend/index.tsx
+++ src/frontend/index.tsx
@@ -8,11 +8,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
+import { ErrorBoundary } from "./components/ErrorBoundary";
 
 const elem = document.getElementById("root")!;
 const app = (
   <StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </StrictMode>
 );
 
